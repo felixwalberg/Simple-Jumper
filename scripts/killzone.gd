757d132757caa6cf1e4559ed7d1bfdb8c5020321
Felix Walberg <fwalberg@uvm.edu>
2025-06-28 14:01:07 -0400

    Added enemy, custom movement input

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,11 +3,16 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You fell out of the world!")
+	print("You lost!")
+	# Slow everything down during "death"
+	Engine.time_scale = 0.5
+	# Remove collisions from the player so they fall through the world
+	body.get_node("CollisionShape2D").queue_free()
 	# Start delay to restart world
 	timer.start()
 	
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	# Restarts world
 	get_tree().reload_current_scene()
--- scripts/slime_player.gd
+++ scripts/slime_player.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 var SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 func _speed_potion():
 	speed_potion_duration.start()
 	SPEED = 250
@@ -20,12 +22,18 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get input direction: -1 (left), 0 (idle), or 1 (right)
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip sprite according to direction
+	if direction > 0: 
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true	
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
